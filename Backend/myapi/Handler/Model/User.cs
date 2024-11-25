efd6bd81935cc166074555b96aa98a2c70e2d9a6
TuanyangLT <tuanyang@lentune.com>
2024-11-26 12:52:23 +1300

    add validation for User class

--- Backend/myapi/Handler/Model/User.cs
+++ Backend/myapi/Handler/Model/User.cs
@@ -4,12 +4,14 @@ namespace Handler.Model
 {
 	public class User
 	{
-		[Required]
-		[MaxLength(50)]
+		[Required(ErrorMessage = "First Name is required.")]
+		[MaxLength(50, ErrorMessage = "First Name cannot exceed 50 characters.")]
+		[RegularExpression(@"^(?=.*[A-Za-z])[A-Za-z ]+$", ErrorMessage = "First Name can only contain letters and spaces, and must include at least one letter.")]
 		public string FirstName { get; set; }
 
-		[Required]
-		[MaxLength(50)]
+		[Required(ErrorMessage = "Last Name is required.")]
+		[MaxLength(50, ErrorMessage = "Last Name cannot exceed 50 characters.")]
+		[RegularExpression(@"^(?=.*[A-Za-z])[A-Za-z ]+$", ErrorMessage = "Last Name can only contain letters and spaces, and must include at least one letter.")]
 		public string LastName { get; set; }
 	}
 
